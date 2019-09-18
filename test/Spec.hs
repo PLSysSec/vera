1570b8a4492d1817749a50e762566473fc8106e0
mlfbrown <mlfbrown@stanford.edu>
2019-09-18 14:17:36 -0700

    more more more

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import           BenchUtils
+import           IonMonkey
+import           Test.Tasty
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $ testGroup "All tests" $ map getTest allTests
+
+allTests :: [BenchTest]
+allTests = [ ionMonkeyTests ]
+
