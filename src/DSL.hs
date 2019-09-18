29bff2e405d2d999be7703c1f9e0b058132af8b1
mlfbrown <mlfbrown@stanford.edu>
2019-09-18 13:03:42 -0700

    comparison functions

--- src/DSL.hs
+++ src/DSL.hs
@@ -1,36 +1,73 @@
 module DSL where
+import           Prelude hiding (max, min)
 import qualified Wrapper as B
 
+-- Standard sorts
 
+i64 :: (B.MonadBoolector m) => m B.Sort
+i64 = B.bitvecSort 64
 
--- Integers constants
+i32 :: (B.MonadBoolector m) => m B.Sort
+i32 = B.bitvecSort 32
+
+i16 :: (B.MonadBoolector m) => m B.Sort
+i16 = B.bitvecSort 16
+
+i8 :: (B.MonadBoolector m) => m B.Sort
+i8 = B.bitvecSort 8
+
+i1 :: (B.MonadBoolector m) => m B.Sort
+i1 = B.bitvecSort 1
+
+-- Integer consts
 
 -- | 64-bit constant
-i64 :: (B.MonadBoolector m) => Integer -> m B.Node
-i64 val | val <= 18446744073709551615 = B.bitvecSort 64 >>= B.unsignedInt val
+i64c :: (B.MonadBoolector m) => Integer -> m B.Node
+i64c val | val <= 18446744073709551615 = i64 >>= B.unsignedInt val
         | otherwise = error $ unwords $ [show val, "is past the range of i64s"]
 
 -- | 32-bit constant
-i32 :: (B.MonadBoolector m) => Integer -> m B.Node
-i32 val | val <= 4294967295 = B.bitvecSort 32 >>= B.unsignedInt val
+i32c :: (B.MonadBoolector m) => Integer -> m B.Node
+i32c val | val <= 4294967295 = i32 >>= B.unsignedInt val
         | otherwise = error $ unwords $ [show val, "is past the range of i32s"]
 
 -- | 16-bit constant
-i16 :: (B.MonadBoolector m) => Integer -> m B.Node
-i16 val | val <= 65535 = B.bitvecSort 16 >>= B.unsignedInt val
+i16c :: (B.MonadBoolector m) => Integer -> m B.Node
+i16c val | val <= 65535 = i16 >>= B.unsignedInt val
         | otherwise = error $ unwords $ [show val, "is past the range of i16s"]
 
 -- | 8-bit constant
-i8 :: (B.MonadBoolector m) => Integer -> m B.Node
-i8 val | val <= 255 = B.bitvecSort 8 >>= B.unsignedInt val
+i8c :: (B.MonadBoolector m) => Integer -> m B.Node
+i8c val | val <= 255 = i8 >>= B.unsignedInt val
        | otherwise = error $ unwords $ [show val, "is past the range of i8s"]
 
 -- | 1-bit constant
-i1 :: (B.MonadBoolector m) => Integer -> m B.Node
-i1 val | val <= 1 = B.bitvecSort 1 >>= B.unsignedInt val
+i1c :: (B.MonadBoolector m) => Integer -> m B.Node
+i1c val | val <= 1 = i1 >>= B.unsignedInt val
        | otherwise = error $ unwords $ [show val, "is past the range of i1s"]
 
--- Floating point constants
+-- Functions
+
+smin :: (B.MonadBoolector m) => B.Node -> B.Node -> m B.Node
+smin x y = do
+  isLess <- B.slte x y
+  B.cond isLess x y
+
+smax :: (B.MonadBoolector m) => B.Node -> B.Node -> m B.Node
+smax x y = do
+  isMore <- B.sgte x y
+  B.cond isMore x y
+
+umin :: (B.MonadBoolector m) => B.Node -> B.Node -> m B.Node
+umin x y = do
+  isLess <- B.ulte x y
+  B.cond isLess x y
+
+umax :: (B.MonadBoolector m) => B.Node -> B.Node -> m B.Node
+umax x y = do
+  isMore <- B.ugte x y
+  B.cond isMore x y
+
 
 
 
