75d7218d7115be6997c5c88558e0b7604c811387
mlfbrown <mlfbrown@stanford.edu>
2019-09-19 15:05:07 -0700

    slightly nicer way of having rangers

---
+++ src/IonMonkeyObjects.hs
@@ -0,0 +1,40 @@
+module IonMonkeyObjects ( Range
+                        , lower
+                        , upper
+                        , newRange
+                        , operandWithRange
+                        , verifyInRange
+                        ) where
+import qualified DSL as D
+
+-- IonMonkey's range object
+-- https://searchfox.org/mozilla-central/source/js/src/jit/RangeAnalysis.h#119
+data Range = Range {
+      name  :: String
+    , lower :: D.Node
+    , upper :: D.Node
+    }
+
+newRange :: (D.MonadBoolector m) => String -> m D.Sort -> m Range
+newRange operandName sort = do
+  let lowerName = operandName ++ "_lower"
+      upperName = operandName ++ "_upper"
+  lowerNode <- D.var' sort lowerName
+  upperNode <- D.var' sort upperName
+  D.slte lowerNode upperNode >>= D.assert
+  return $ Range operandName lowerNode upperNode
+
+operandWithRange :: (D.MonadBoolector m) => String -> m D.Sort -> Range -> m D.Node
+operandWithRange name sort range = do
+  operand <- D.var' sort name
+  verifyInRange operand range
+  return operand
+
+verifyInRange :: (D.MonadBoolector m) => D.Node -> Range -> m ()
+verifyInRange node range = do
+  D.slte node (upper range) >>= D.assert
+  D.sgte node (lower range) >>= D.assert
+
+
+
+
--- test/IonMonkey.hs
+++ test/IonMonkey.hs
@@ -2,6 +2,7 @@ module IonMonkey where
 import           BenchUtils
 import           Control.Monad.State.Strict (liftIO)
 import qualified DSL                        as D
+import           IonMonkeyObjects
 import           Test.Tasty
 import           Test.Tasty.Golden
 import           Test.Tasty.HUnit
@@ -17,53 +18,43 @@ lsh :: BenchTest
 lsh = benchTestCase "lsh" $ do
   bs <- D.newBoolectorState Nothing
   result <- D.evalBoolector bs $ do
-    -- Compute the shift amount
+    -- We are shifting by a constant, compute the shift amount
     c <- D.i32v "c"
     const <- D.i32c 31
     shift <- D.and c const
 
-    -- Setup the current bounds on the range
-    lower <- D.i32v "lower bound"
-    upper <- D.i32v "upper bound"
-    finalLower <- D.i32v "final lower bound"
-    finalUpper <- D.i32v "final upper bound"
-    D.slte lower upper >>= D.assert
-    D.slte finalLower finalUpper >>= D.assert
+    -- Create the other input range and the output range
+    startRange <- newRange "start range" D.i32
+    endRange <- newRange "end range" D.i32
 
     -- Do their if-checking:
     -- lower << shift << 1 >> shift >> 1 == lower &&
     -- upper << shift << 1 >> shift >> 1 == upper
     -- range is lower << shift, upper << shift
     -- Lower
-    t1 <- D.safeSll lower shift
+    t1 <- D.safeSll (lower startRange) shift
     t2 <- D.i32c 1 >>= D.safeSll t1
     t3 <- D.safeSrl t2 shift
     t4 <- D.i32c 1 >>= D.safeSrl t3
-    check1 <- D.eq t4 lower
+    check1 <- D.eq t4 (lower startRange)
     -- Upper
-    t5 <- D.safeSll upper shift
+    t5 <- D.safeSll (upper startRange) shift
     t6 <- D.i32c 1 >>= D.safeSll t5
     t7 <- D.safeSll t6 shift
     t8 <- D.i32c 1 >>= D.safeSll t7
-    check2 <- D.eq t8 upper
+    check2 <- D.eq t8 (upper startRange)
 
     -- Set new range bounds
     defaultLower <- D.i32min
     defaultUpper <- D.i32max
-    D.cond check1 t1 defaultLower >>= D.eq finalLower
-    D.cond check2 t5 defaultUpper >>= D.eq finalUpper
+    D.cond check1 t1 defaultLower >>= D.eq (lower endRange)
+    D.cond check2 t5 defaultUpper >>= D.eq (upper endRange)
 
     -- Verify that the operation is always in bounds
-    operand <- D.i32v "the shifted variable"
-    D.slte operand upper >>= D.assert
-    D.sgte operand lower >>= D.assert
+    operand <- operandWithRange "shifted variable" D.i32 startRange
     shiftResult <- D.safeSll operand c
-    D.slte shiftResult finalUpper >>= D.assert
-    D.sgte shiftResult finalLower >>= D.assert
-    result <- D.sat
-    lowerConcrete <- D.signedBvAssignment finalLower
-    liftIO $ print lowerConcrete
-    return result
+    verifyInRange shiftResult endRange
+    D.sat
 
   result @=? D.Sat
 
