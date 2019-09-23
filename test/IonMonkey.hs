d29f1929190e6d6319e6928c67f4e7117fcd51fb
mlfbrown <mlfbrown@stanford.edu>
2019-09-23 12:08:26 -0700

    unsigned

--- test/IonMonkey.hs
+++ test/IonMonkey.hs
@@ -202,16 +202,16 @@ ursh'Test = benchTestCase "ursh'" $ do
     leftRange <- newInputRange "shitee range" D.i32
     rightRange <- newInputRange "shifter range" D.i32
     resultRange <- ursh' leftRange rightRange
-    c1 <- verifySaneRange resultRange
+    c1 <- uVerifySaneRange resultRange
 
     left <- operandWithRange "value to shift" D.i32 leftRange
     right <- operandWithRange "shit by" D.i32 rightRange
     -- Need to mask https://www.ecma-international.org/ecma-262/5.1/#sec-11.7.3
     maskedRight <- D.i32c 31 >>= D.and right
-    result <- D.safeSra left right
+    result <- D.safeSra left maskedRight
 
-    c2 <- verifyUpperBound result resultRange
-    c3 <- verifyLowerBound result resultRange
+    c2 <- uVerifyUpperBound result resultRange
+    c3 <- uVerifyLowerBound result resultRange
 
     return (c1, c2, c3)
 
