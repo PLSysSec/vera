a19f031f004e5d05be7972c9edc790361812f461
mlfbrown <mlfbrown@stanford.edu>
2019-09-23 18:25:54 -0700

    even more

--- src/V8/Operations.hs
+++ src/V8/Operations.hs
@@ -2,6 +2,26 @@ module V8.Operations where
 import qualified DSL.DSL           as D
 import           IonMonkey.Objects
 
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=589
+numberAdd :: Range -> Range -> D.Verif Range
+numberAdd = undefined
+
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=634
+numberSubtract :: Range -> Range -> D.Verif Range
+numberSubtract = undefined
+
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=697
+numberMultiply :: Range -> Range -> D.Verif Range
+numberMultiply = undefined
+
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=742
+numberDivide :: Range -> Range -> D.Verif Range
+numberDivide = undefined
+
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=771
+numberModulus :: Range -> Range -> D.Verif Range
+numberModulus = undefined
+
 -- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=840
 numberBitwiseOr :: Range -> Range -> D.Verif Range
 numberBitwiseOr = undefined
