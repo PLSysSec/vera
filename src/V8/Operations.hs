04b51842cc3e43361bd09cef9ee6515d03a87b52
mlfbrown <mlfbrown@stanford.edu>
2019-09-23 19:30:24 -0700

    more

--- src/V8/Operations.hs
+++ src/V8/Operations.hs
@@ -85,7 +85,28 @@ numberShiftLeft = undefined
 
 -- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=968
 numberShiftRight :: Range -> Range -> D.Verif Range
-numberShiftRight = undefined
+numberShiftRight left right = do
+  thirtyOne <- D.i32c 31
+  zero <- D.i32c 0
+
+  rightLarger <- D.sgt (upper right) thirtyOne
+  maxRight <- D.cond rightLarger thirtyOne (upper right)
+  minRight <- D.cond rightLarger zero (lower right)
+
+  min <- do
+    firstShift <- D.safeSra (lower left) minRight
+    secondShift <- D.safeSra (lower left) maxRight
+    D.smin firstShift secondShift
+
+  max <- do
+    firstShift <- D.safeSra (upper left) minRight
+    secondShift <- D.safeSra (upper left) maxRight
+    D.smin firstShift secondShift
+
+  result <- newResultRange "result" D.i32
+  D.assign (lower result) min
+  D.assign (upper result) max
+  return result
 
 -- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=993
 numberShiftRightLogical :: Range -> Range -> D.Verif Range
--- test/V8.hs
+++ test/V8.hs
@@ -8,7 +8,9 @@ import           Test.Tasty.HUnit
 import           V8.Operations
 
 v8Tests :: BenchTest
-v8Tests = benchTestGroup "V8 tests" [ andTest ]
+v8Tests = benchTestGroup "V8 tests" [ andTest
+                                    , rshTest
+                                    ]
 
 andTest :: BenchTest
 andTest = benchTestCase "and" $ do
@@ -30,4 +32,26 @@ andTest = benchTestCase "and" $ do
   Verified @=? c2
   Verified @=? c3
 
+rshTest :: BenchTest
+rshTest = benchTestCase "rsh" $ do
+  (c1, c2, c3) <- D.evalVerif Nothing $ do
 
+    leftRange <- newInputRange "shitee range" D.i32
+    rightRange <- newInputRange "shifter range" D.i32
+    resultRange <- numberShiftRight leftRange rightRange
+    c1 <- verifySaneRange resultRange
+
+    left <- operandWithRange "value to shift" D.i32 leftRange
+    right <- operandWithRange "shit by" D.i32 rightRange
+    -- Need to mask https://www.ecma-international.org/ecma-262/5.1/#sec-11.7.3
+    maskedRight <- D.i32c 31 >>= D.and right
+    result <- D.safeSra left maskedRight
+
+    c2 <- verifyUpperBound result resultRange
+    c3 <- verifyLowerBound result resultRange
+
+    return (c1, c2, c3)
+
+  Verified @=? c1
+  Verified @=? c2
+  Verified @=? c3
