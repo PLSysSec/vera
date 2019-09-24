7504e2ac313908d8652335254e7cd63ea01c80ae
mlfbrown <mlfbrown@stanford.edu>
2019-09-23 18:19:29 -0700

    more

--- src/V8/Operations.hs
+++ src/V8/Operations.hs
@@ -2,5 +2,27 @@ module V8.Operations where
 import qualified DSL.DSL           as D
 import           IonMonkey.Objects
 
-and :: Range -> Range -> D.Verif Range
-and = undefined
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=840
+numberBitwiseOr :: Range -> Range -> D.Verif Range
+numberBitwiseOr = undefined
+
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=877
+numberBitwiseAnd :: Range -> Range -> D.Verif Range
+numberBitwiseAnd = undefined
+
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=908
+numberBitwiseXor :: Range -> Range -> D.Verif Range
+numberBitwiseXor = undefined
+
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=933
+numberShiftLeft :: Range -> Range -> D.Verif Range
+numberShiftLeft = undefined
+
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=968
+numberShiftRight :: Range -> Range -> D.Verif Range
+numberShiftRight = undefined
+
+-- | https://cs.chromium.org/chromium/src/v8/src/compiler/operation-typer.cc?q=NumberAdd&sq=package:chromium&g=0&l=993
+numberShiftRightLogical :: Range -> Range -> D.Verif Range
+numberShiftRightLogical = undefined
+
