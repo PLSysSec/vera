a692a8e479fe45374378528cc0b4124dd926ac6a
mlfbrown <mlfbrown@stanford.edu>
2019-10-01 13:11:37 -0700

    tests for cpp

--- test/Cpp.hs
+++ test/Cpp.hs
@@ -1,21 +1,29 @@
-module Cpp where
+module Cpp (cppTests) where
 import           BenchUtils
 import           Control.Monad.State.Strict (liftIO)
+import qualified Data.Map                   as M
 import qualified DSL.DSL                    as D
+import qualified DSL.Typed                  as T
 import           IonMonkey.Objects
 import           IonMonkey.Operations
 import           Prelude                    hiding (and, not)
 import           Test.Tasty.HUnit
+import           Utils
 
 cppTests :: BenchTest
-cppTests = benchTestGroup "C++ tests" []
+cppTests = benchTestGroup "C++ tests" [ cppMinTest ]
 
--- cppMinTest :: BenchTest
--- cppMinTest = benchTestCase "Cpp min" $ do
---   (c1, c2, c3) <- D.evalVerif Nothing $ do
+cppMinTest :: BenchTest
+cppMinTest = benchTestCase "min test" $ do
 
---     return (c1, c2, c3)
+  (r) <- D.evalVerif Nothing $ do
 
---   Verified @=? c1
---   Verified @=? c2
---   Verified @=? c3
+    left <- T.int32 "left"
+    right <- T.int32 "right"
+    result <- T.cppMin left right
+    D.runSolver
+
+  vtest "cppMin" r $ M.fromList [ ("left", 0)
+                                , ("right", 0)
+                                ]
+
