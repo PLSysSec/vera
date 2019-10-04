dafff10187879842eea92865640c204ff6d68e5f
mlfbrown <mlfbrown@stanford.edu>
2019-10-03 20:12:23 -0700

    goodbye v8 we do not need you right now

--- src/V8/Objects.hs
+++
@@ -1,1 +0,0 @@
-module V8.Objects where
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,14 +2,12 @@ import           BenchUtils
 import           Cpp
 import           IonMonkey
 import           Test.Tasty
-import           V8
 
 main :: IO ()
 main = defaultMain $ testGroup "All tests" $ map getTest allTests
 
 allTests :: [BenchTest]
 allTests = [ ionMonkeyTests
-           , v8Tests
            , cppTests
            ]
 
