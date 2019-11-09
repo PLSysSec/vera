329d12244cfeb637a46a4b0330b1b5c1bf38a570
David Thien <dthien@eng.ucsd.edu>
2019-11-09 15:46:49 -0800

    Added test to generate all functions and the range object

--- test/CppGen.hs
+++ test/CppGen.hs
@@ -16,7 +16,7 @@ import           Prelude                       hiding (abs, and, floor, max,
                                                 min, not, or)
 import           Test.Tasty.HUnit
 import           Utils
-import           Data.List
+import           Data.List                     (intercalate)
 import           System.Directory              (createDirectoryIfMissing)
 
 cppGenTests :: BenchTest
@@ -26,6 +26,7 @@ cppGenTests = benchTestGroup "CPP Gen tests"
               , cppAddTest
               , cppSubTest
               , helloWorldTest
+              , genAllFunctionsTest
               ]
 
 writeCompiled :: String -> [String] -> Codegen ()
@@ -64,6 +65,173 @@ cppSubTest = benchTestCase "cpp sub test" $ do
     define sub
     compiled <- compileFunction sub
     writeCompiled "test/GenCpp/sub.cpp" compiled
+    error "test"
+  error "test1"
+
+genAllFunctionsTest :: BenchTest
+genAllFunctionsTest = benchTestCase "cpp gen all functions test" $ do
+  r <- evalCodegen Nothing $ do
+    
+    class_ range
+    define add
+    define sub
+    define and
+    define or
+    define xor
+    define not
+    define mul
+    define lsh
+    define rsh
+    define ursh
+    define lsh'
+    define rsh'
+    define ursh'
+    define abs
+    define min
+    define floor
+    define ceil
+    define sign
+    define intersect
+    define range3
+    define range4
+    define range6
+    define newInt32Range
+    define newUInt32Range
+    define setLowerInit
+    define setUpperInit
+    define canHaveSignBitSet
+    define exponentImpliedByInt32Bounds
+    define hasInt32Bounds
+    define numBits
+    define canBeFiniteNonNegative
+    define isFiniteNonNegative
+    define isFiniteNegative
+    define canBeInfiniteOrNan
+    define missingAnyInt32Bounds
+    define canBeNan
+    define canBeZero
+    define contains
+    define countOnes
+    define countLeadingZeroes
+    define newInt32InputRange
+    define newFloatInputRange
+    define intInRange
+    define floatInRange
+    define verifySaneRange
+    define verifyLower
+    define verifyUpper
+    define verifyNegZero
+    define verifyNan
+    define verifyInf
+    define verifyExp
+
+    compiledRange <- compileClass range
+    compiledAdd <- compileFunction add
+    compiledSub <- compileFunction sub
+    compiledAnd <- compileFunction and
+    compiledOr <- compileFunction or
+    compiledXor <- compileFunction xor
+    compiledNot <- compileFunction not
+    compiledMul <- compileFunction mul
+    compiledLsh <- compileFunction lsh
+    compiledRsh <- compileFunction rsh
+    compiledUrsh <- compileFunction ursh
+    compiledLsh <- compileFunction lsh'
+    compiledRsh <- compileFunction rsh'
+    compiledUrsh <- compileFunction ursh'
+    compiledAbs <- compileFunction abs
+    compiledMin <- compileFunction min
+    compiledFloor <- compileFunction floor
+    compiledCeil <- compileFunction ceil
+    compiledSign <- compileFunction sign
+    compiledIntersect <- compileFunction intersect
+    compiledRange3 <- compileFunction range3
+    compiledRange4 <- compileFunction range4
+    compiledRange6 <- compileFunction range6
+    compiledNewInt32Range <- compileFunction newInt32Range
+    compiledNewUInt32Range <- compileFunction newUInt32Range
+    compiledSetLowerInit <- compileFunction setLowerInit
+    compiledSetUpperInit <- compileFunction setUpperInit
+    compiledCanHaveSignBitSet <- compileFunction canHaveSignBitSet
+    compiledExponentImpliedByInt32Bounds <- compileFunction exponentImpliedByInt32Bounds
+    compiledHasInt32Bounds <- compileFunction hasInt32Bounds
+    compiledNumBits <- compileFunction numBits
+    compiledCanBeFiniteNonNegative <- compileFunction canBeFiniteNonNegative
+    compiledIsFiniteNonNegative <- compileFunction isFiniteNonNegative
+    compiledIsFiniteNegative <- compileFunction isFiniteNegative
+    compiledCanBeInfiniteOrNan <- compileFunction canBeInfiniteOrNan
+    compiledMissingAnyInt32Bounds <- compileFunction missingAnyInt32Bounds
+    compiledCanBeNan <- compileFunction canBeNan
+    compiledCanBeZero <- compileFunction canBeZero
+    compiledContains <- compileFunction contains
+    compiledCountOnes <- compileFunction countOnes
+    compiledCountLeadingZeroes <- compileFunction countLeadingZeroes
+    compiledNewInt32InputRange <- compileFunction newInt32InputRange
+    compiledNewFloatInputRange <- compileFunction newFloatInputRange
+    compiledIntInRange <- compileFunction intInRange
+    compiledFloatInRange <- compileFunction floatInRange
+    compiledVerifySaneRange <- compileFunction verifySaneRange
+    compiledVerifyLower <- compileFunction verifyLower
+    compiledVerifyUpper <- compileFunction verifyUpper
+    compiledVerifyNegZero <- compileFunction verifyNegZero
+    compiledVerifyNan <- compileFunction verifyNan
+    compiledVerifyInf <- compileFunction verifyInf
+    compiledVerifyExp <- compileFunction verifyExp
+
+    let compiled = concat [ compiledRange
+                          , compiledAdd
+                          , compiledSub
+                          , compiledAnd
+                          , compiledOr
+                          , compiledXor
+                          , compiledNot
+                          , compiledMul
+                          , compiledLsh
+                          , compiledRsh
+                          , compiledUrsh
+                          , compiledLsh
+                          , compiledRsh
+                          , compiledUrsh
+                          , compiledAbs
+                          , compiledMin
+                          , compiledFloor
+                          , compiledCeil
+                          , compiledSign
+                          , compiledIntersect
+                          , compiledRange3
+                          , compiledRange4
+                          , compiledRange6
+                          , compiledNewInt32Range
+                          , compiledNewUInt32Range
+                          , compiledSetLowerInit
+                          , compiledSetUpperInit
+                          , compiledCanHaveSignBitSet
+                          , compiledExponentImpliedByInt32Bounds
+                          , compiledHasInt32Bounds
+                          , compiledNumBits
+                          , compiledCanBeFiniteNonNegative
+                          , compiledIsFiniteNonNegative
+                          , compiledIsFiniteNegative
+                          , compiledCanBeInfiniteOrNan
+                          , compiledMissingAnyInt32Bounds
+                          , compiledCanBeNan
+                          , compiledCanBeZero
+                          , compiledContains
+                          , compiledCountOnes
+                          , compiledCountLeadingZeroes
+                          , compiledNewInt32InputRange
+                          , compiledNewFloatInputRange
+                          , compiledIntInRange
+                          , compiledFloatInRange
+                          , compiledVerifySaneRange
+                          , compiledVerifyLower
+                          , compiledVerifyUpper
+                          , compiledVerifyNegZero
+                          , compiledVerifyNan
+                          , compiledVerifyInf
+                          , compiledVerifyExp]
+
+    writeCompiled "test/GenCpp/all_functions.cpp" compiled
     error "test"
   error "test1"
 
