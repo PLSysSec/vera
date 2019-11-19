cf306e4c314b4155f52facdd33cf36b666027c90
Deian Stefan <deian@cs.ucsd.edu>
2019-11-18 16:16:08 -0800

    intersection port

--- src/IonMonkeyGenerated/Operations.hs
+++ src/IonMonkeyGenerated/Operations.hs
@@ -514,52 +514,38 @@ sign = [funcStr| range sign(range op) {
 
 -- | https://searchfox.org/mozilla-central/source/js/src/jit/RangeAnalysis.cpp#485
 intersect :: FunctionDef
-intersect =
-  let args = [ ("lhs", c "range")
-             , ("rhs", c "range")
-             ]
-      body = [ declare (t Signed) "newLower"
-             , declare (t Signed) "newUpper"
-             , declare (t Bool) "emptyRange"
-             , declare (t Bool) "welter"
-             , v "emptyRange" `assign` n Bool 0
-             , v "newLower" `assign` max_ (v "lhs" .->. "lower") (v "rhs" .->. "lower")
-             , v "newUpper" `assign` min_ (v "lhs" .->. "upper") (v "rhs" .->. "upper")
-             , if_ (v "newUpper" .<. v "newLower")
-               [ if_ ((not_ $ call "canBeNan" [v "lhs"]) .&&. (not_ $ call "canBeNan" [v "rhs"]))
-                 [v "emptyRange" `assign` n Bool 1] []
-               , return_ $ call "nullRange" [v "emptyRange"]
-               ]  []
-             , declare (t Bool) "newHasInt32LowerBound"
-             , declare (t Bool) "newHasInt32UpperBound"
-             , declare (t Bool) "newMayIncludeNegativeZero"
-             , declare (t Bool) "newCanHaveFractionalPart"
-             , declare (t Unsigned16) "newExponent"
+intersect = [funcStr| range intersect(range& lhs, range& rhs){
+   int32_t newLower = math::max(lhs.lower, rhs.lower);
+   int32_t newUpper = math::min(lhs.upper, rhs.upper);
+   bool  emptyRange = (bool) 0;
 
-             , v "newHasInt32LowerBound" `assign` ((v "lhs" .->. "hasInt32LowerBound") .||. (v "rhs" .->. "hasInt32LowerBound"))
-             , v "newHasInt32UpperBound" `assign` ((v "lhs" .->. "hasInt32UpperBound") .||. (v "rhs" .->. "hasInt32UpperBound"))
+   if (newUpper < newLower) {
+     if (!canBeNan(lhs) | !canBeNan(rhs)) {
+       emptyRange = (bool) 1;
+     }
+     return nullRange(emptyRange);
+   }
+   bool newHasInt32LowerBound = lhs.hasInt32LowerBound | rhs.hasInt32LowerBound;
+   bool newHasInt32UpperBound = lhs.hasInt32UpperBound | rhs.hasInt32UpperBound;
+   bool newMayIncludeNegativeZero = lhs.canBeNegativeZero & lhs.canBeNegativeZero;
+   bool newCanHaveFractionalPart = lhs.canHaveFractionalPart & rhs.canHaveFractionalPart;
+   uint16_t newExponent = math::min(lhs.maxExponent, rhs.maxExponent);
 
-             , v "newCanHaveFractionalPart" `assign` ((v "lhs" .->. "canHaveFractionalPart") .&&. (v "rhs" .->. "canHaveFractionalPart"))
+   if (newHasInt32LowerBound & newHasInt32UpperBound & (newExponent == #{includesInfinityAndNanS})) {
+       return nullRange(emptyRange);
+   }
 
-             , v "newMayIncludeNegativeZero" `assign` ( (v "lhs" .->. "canBeNegativeZero") .&&. (v "lhs" .->. "canBeNegativeZero") )
-
-             , v "newExponent" `assign` min_ (v "lhs" .->. "maxExponent") (v "rhs" .->. "maxExponent")
-
-
-             , if_ (v "newHasInt32LowerBound" .&&. v "newHasInt32UpperBound" .&&. (v "newExponent" .==. includesInfinityAndNan)) [return_ $ call "nullRange" [v "emptyRange"]] []
-
-             , declare (c "range") "intersect_result"
-             , v "intersect_result" .->. "lower" `assign` v "newLower"
-             , v "intersect_result" .->. "hasInt32LowerBound" `assign` v "newHasInt32LowerBound"
-             , v "intersect_result" .->. "upper" `assign` v "newUpper"
-             , v "intersect_result" .->. "hasInt32UpperBound" `assign` v "newHasInt32UpperBound"
-             , v "intersect_result" .->. "canBeNegativeZero" `assign` v "newMayIncludeNegativeZero"
-             , v "intersect_result" .->. "canHaveFractionalPart" `assign` v "newCanHaveFractionalPart"
-             , v "intersect_result" .->. "maxExponent" `assign` v "newExponent"
-             , v "intersect_result" .->. "isEmpty" `assign` v "emptyRange"
-             , return_ $ v "intersect_result"
-             ]
-   in Function "intersect" (c "range") args body
+   range intersect_result;
+   intersect_result.lower = newLower;
+   intersect_result.hasInt32LowerBound = newHasInt32LowerBound;
+   intersect_result.upper = newUpper;
+   intersect_result.hasInt32UpperBound = newHasInt32UpperBound;
+   intersect_result.canBeNegativeZero = newMayIncludeNegativeZero;
+   intersect_result.canHaveFractionalPart = newCanHaveFractionalPart;
+   intersect_result.maxExponent = newExponent;
+   intersect_result.isEmpty = emptyRange;
+   return intersect_result;
+}|]
 
 -- | https://searchfox.org/mozilla-central/source/js/src/jit/RangeAnalysis.cpp#485
 brokenIntersect :: FunctionDef
