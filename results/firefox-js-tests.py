1b4fd525cd14f0c1bb366eeaa27c9e4e459f9f59
John Renner <john@jrenner.net>
2020-02-29 00:11:58 -0800

    Cleaned up JS stuff

--- results/firefox-js-tests.py
+++ results/firefox-js-tests.py
@@ -1,3 +1,22 @@
-with open("firefox-js-tests.txt", 'w') as file:
-    file.write("bleh")
-raise Exception("Not implemented")
+import os
+from subprocess import Popen, PIPE
+
+def pipe_out(cmd):
+    os.system(cmd + " >> firefox-js-tests")
+
+def tee(s):
+    print(s)
+    pipe_out("echo \"" + s + "\"")
+
+os.system("echo '' > firefox-js-tests.txt")
+tee("Running JSAPI Tests...")
+#pipe_out("./mach jsapik)
+pipe_out("echo foo")
+tee("Running JIT-Tests...")
+#pipe_out("./mach jit-test")
+pipe_out("echo foo")
+tee("Running JS Tests... (takes ~30min)")
+#pipe_out("./mach jstests")
+pipe_out("echo foo")
+
+print("Results written to firefox-js-tests.txt")
--- results/sanity.py
+++ results/sanity.py
@@ -1,4 +1,5 @@
 import os
 print("Running sanity checks")
 os.system("stack test --ta '-p Sanity' &> verif_sanity.txt")
+#os.system("./mach jstests test262/harness/nans.js")
 print("---> Wrote output to results/verify_sanity.txt")
